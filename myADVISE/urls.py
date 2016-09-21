f617bf94ccd584caedda623ee03b0a52503e35bc
clfull01 <clfull01@gmail.com>
2016-09-21 17:37:51 -0400

    Fix url pathing

--- myADVISE/urls.py
+++ myADVISE/urls.py
@@ -16,11 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myADVISE.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^$', include("basic.urls")),
-)
+]
