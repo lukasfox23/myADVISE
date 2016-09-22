a10bb8315cb03d4f8e3756c6ccf0242348be78af
clfull01 <clfull01@gmail.com>
2016-09-22 17:59:07 -0400

    Adjust heroku server info

--- myADVISE/settings.py
+++ myADVISE/settings.py
@@ -80,11 +80,16 @@ WSGI_APPLICATION = 'myADVISE.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'NAME': 'd44hau4ahenrou',                      # Or path to database file if using sqlite3.
+            # The following settings are not used with sqlite3:
+            'USER': 'vpbdqdgijspxrm',
+            'PASSWORD': 'L3l8Agiq-Bcae1VFL7VGrYbizX',
+            'HOST': 'ec2-54-225-195-254.compute-1.amazonaws.com',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+            'PORT': '5432',                      # Set to empty string for default.
+        }
     }
-}
 
 AUTH_PASSWORD_VALIDATORS = [
     {
