183275fc9eafac71e85147b88b758be7891a3fcc
clfull01 <clfull01@gmail.com>
2016-09-22 16:18:49 -0400

    I r deploy mastr

--- myADVISE/urls.py
+++ myADVISE/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'myADVISE.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', include("basic.urls")),
-)
+]
