ff1f6c155f61631ca65fed4df7469014b111280d
clfull01 <clfull01@gmail.com>
2016-09-22 23:00:04 -0400

    Added student database outline

--- basic/models.py
+++ basic/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    StudentID = models.AutoField(primary_key=True)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    major = models.CharField(max_length=50)
+    graduationDate = models.DateField()
+    class Meta:
+        managed = False
+        db_table = 'basic_student'
+    def __str__(self):  # __unicode__ on Python 2
+        return self.firstname + self.lastname
+
+# STUDENT(StudentId, FirstName, LastName, School, GraduationDate)
+# COURSE(CourseId, CourseCode, CourseName, School, InstructorId, GenEdFlag, DualCredit, Mon, Tues, Wed, Thu, Fri, Sat, Sun)
+# INSTRUCTOR(InstructorId, FirstName, LastName, CourseId, Rating)
+# PROGRESS(ProgressId, StudentId, CourseId, CompletionFlag, OralCommunication, Humanities, Arts, CulturalDiversity1, CulturalDiversity2, SocialSciences)
--- basic/views.py
+++ basic/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-
+from basic.models import Student
 # Create your views here.
 def index(request):
+
     return render(request, "basic/basic.html")
 
 def about(request):
-    return render(request, "basic/about.html")
+    user_list = Student.objects.all()[:50]
+    return render(request, "basic/about.html", {'users': user_list})
