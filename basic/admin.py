0d888ce56ac6f35edf0d6a3f17cda52b2bde0547
clfull01 <clfull01@gmail.com>
2016-10-03 12:09:52 -0400

    Add admin panels for rest of database

--- basic/admin.py
+++ basic/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 from basic.models import Student
+from basic.models import Instructor
+from basic.models import Course
+from basic.models import Progress
 
 # Register your models here.
 admin.site.register(Student)
+admin.site.register(Instructor)
+admin.site.register(Course)
+admin.site.register(Progress)
--- basic/models.py
+++ basic/models.py
@@ -12,6 +12,29 @@ class Student(models.Model):
         db_table = 'student'
     def __str__(self):  # __unicode__ on Python 2
         return self.firstname + " " + self.lastname
+class Instructor(models.Model):
+    instructorid = models.AutoField(primary_key=True)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    rating = models.FloatField()
+
+class Course(models.Model):
+    courseid = models.AutoField(primary_key=True)
+    coursecode = models.CharField(max_length=10)
+    coursename = models.CharField(max_length=50)
+    school = models.CharField(max_length=50)
+    instructorid = models.ForeignKey(Instructor)
+    genedflag = models.BooleanField(default=False)
+    coursetime = models.CharField(max_length=200)
+
+class Progress(models.Model):
+    progressid = models.AutoField(primary_key=True)
+    studentid = models.ForeignKey(Student)
+    courseid = models.ForeignKey(Course)
+    completionflag = models.BooleanField(default=False)
+    genedlist = models.CharField(max_length=200)
+
+
 
 # STUDENT(StudentId, FirstName, LastName, School, GraduationDate)
 # COURSE(CourseId, CourseCode, CourseName, School, InstructorId, GenEdFlag, DualCredit, Mon, Tues, Wed, Thu, Fri, Sat, Sun)
