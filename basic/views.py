2c53d43293d35ecc2f9b7bedbed94a5e76b5abf3
clfull01 <clfull01@gmail.com>
2016-11-05 11:39:52 -0400

    Complete registration authentication

--- basic/views.py
+++ basic/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from basic.models import Student
+from myADVISE.forms import UserForm
+#login view request is named login...renamed default django function for clairty
+from django.contrib.auth import login as auth_login
 # Create your views here.
 @login_required(login_url='login/')
 def index(request):
@@ -14,3 +16,15 @@ def about(request):
 
 def login(request):
     return render(request, "basic/login.html")
+
+def create(request):
+    if request.method == "POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            new_user = User.objects.create_user(**form.cleaned_data)
+            auth_login(request, new_user)
+            # redirect, or however you want to get to the main view
+            return render(request, "basic/basic.html")
+    else:
+        form = UserForm()
+        return render(request, 'basic/create.html', {'form': form})
--- myADVISE/forms.py
+++ myADVISE/forms.py
@@ -1,8 +1,18 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
+from django.forms import ModelForm
 from django import forms
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(label="Username", max_length=30,
                                widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
+    password = forms.CharField(label="Password", max_length=30,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password', 'type':'password'}))
+
+class UserForm(forms.Form):
+    username = forms.CharField(label="Username", max_length=30,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
+    email = forms.CharField(label="Email", max_length=30,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'email'}))
     password = forms.CharField(label="Password", max_length=30,
                                widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'password', 'type':'password'}))
