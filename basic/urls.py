7fd3732e458eafb879b67b0517e0dc50b019d7b5
Derrek <dabaxt01@gmail.com>
2016-11-19 18:31:39 -0500

    Created a placeholder for profiles, edited header, dropped contact

--- basic/urls.py
+++ basic/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^create/$', views.create, name='create'),
+    url(r'^profile/$', views.profile, name='profile'),
 ]
