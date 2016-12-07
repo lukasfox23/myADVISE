2474384e0e7890b4c69063d9731d18a37f7dc678
clfull01 <clfull01@gmail.com>
2016-12-07 18:09:30 -0500

    Remove debug

--- myADVISE/settings.py
+++ myADVISE/settings.py
@@ -25,7 +25,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = "(i*!j@rkryus2wpn-z*y5=8=*jn+-om=fg@*sz!bwl*d*f(2zt"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Application definition
 
